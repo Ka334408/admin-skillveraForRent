200660f02883b0409073fa57a6330da514ddce07
Ka334408 <104441539+Ka334408@users.noreply.github.com>
2026-01-22 23:37:46 +0200

    .

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,19 +1,22 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useLocale } from "next-intl";
-import { redirect, usePathname, useRouter } from "next/navigation";
-
+import { usePathname, useRouter } from "next/navigation";
 
 export default function HomePage() {
     const locale = useLocale();
     const router = useRouter();
     const pathname = usePathname();
 
-    const isMobileView = pathname.includes("mobile");
+    useEffect(() => {
+        const hostname = window.location.hostname;
 
-    if (isMobileView) {
-        redirect(`/${locale}/mobile/auth/login`);
-    } else  {
-        redirect(`/${locale}/auth/login`)
-    }
+        if (hostname.includes("mobile")) {
+            router.replace(`/${locale}/mobile/auth/login`);
+        } else {
+            router.replace(`/${locale}/auth/login`);
+        }
+    }, [locale, router]);
+
+    return null; 
 }
